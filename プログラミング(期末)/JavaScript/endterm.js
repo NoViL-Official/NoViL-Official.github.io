fcfcddd7146c933c61e356573a678a202f6956eb
NoViL-Official <68681988+NoViL-Official@users.noreply.github.com>
2020-07-30 23:35:04 +0900

    Update endterm.js

--- プログラミング(期末)/JavaScript/endterm.js
+++ プログラミング(期末)/JavaScript/endterm.js
@@ -1,13 +1,13 @@
 $("#top-page").click (function (){
-    window.location.href = 'top-page.html';
+    window.location.href = 'https://novil-official.github.io/%E3%83%95%E3%82%9A%E3%83%AD%E3%82%AF%E3%82%99%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%AF%E3%82%99(%E6%9C%9F%E6%9C%AB)/top-page.html';
 });
 
 $("#about-music").click (function () {
-   window.location.href = 'about-music.html'; 
+   window.location.href = 'https://novil-official.github.io/%E3%83%95%E3%82%9A%E3%83%AD%E3%82%AF%E3%82%99%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%AF%E3%82%99(%E6%9C%9F%E6%9C%AB)/about-music.html'; 
 });
 
 $("#link-navigation").click (function () {
-   window.location.href = 'link-navigation.html'; 
+   window.location.href = 'https://novil-official.github.io/%E3%83%95%E3%82%9A%E3%83%AD%E3%82%AF%E3%82%99%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%AF%E3%82%99(%E6%9C%9F%E6%9C%AB)/link-navigation.html'; 
 });
 
 function charactersize(size) {

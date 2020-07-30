a463c2bdd8fb6db6accb69d08e70099042d10bc4
NoViL-Official <68681988+NoViL-Official@users.noreply.github.com>
2020-07-30 23:31:43 +0900

    Update endterm.js

--- プログラミング(期末)/JavaScript/endterm.js
+++ プログラミング(期末)/JavaScript/endterm.js
@@ -1,13 +1,13 @@
 $("#top-page").click (function (){
-    window.location.href = '%E3%83%88%E3%83%83%E3%83%97%E3%83%9A%E3%83%BC%E3%82%B8.html';
+    window.location.href = 'top-page.html';
 });
 
 $("#about-music").click (function () {
-   window.location.href = '%E9%9F%B3%E6%A5%BD%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6.html'; 
+   window.location.href = 'about-music.html'; 
 });
 
 $("#link-navigation").click (function () {
-   window.location.href = '%E5%90%84%E7%A8%AESNS%E3%81%B8%E3%81%AE%E3%83%AA%E3%83%B3%E3%82%AF.html'; 
+   window.location.href = 'link-navigation.html'; 
 });
 
 function charactersize(size) {
